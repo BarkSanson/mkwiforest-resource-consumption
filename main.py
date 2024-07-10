a9a605efb21cbef7776e6927bd0e2de0a03deb51
Bark Sanson <barksansondev@gmail.com>
2024-07-10 12:08:03 +0200

    Can measure performance

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas as pd
 from monitor import Monitor
 from online_outlier_detection.pipelines import MKWIForestBatchPipeline
 
-INTERVAL = 0.1
-MAX_SAMPLES = 30
+INTERVAL = 0.05
+MAX_SAMPLES = 100
 
 
 def merge_data(date_dir):
@@ -21,44 +21,70 @@ def merge_data(date_dir):
     return df
 
 
+def get_performance(data_list, data_path, window_size, pid):
+    samples = 0
+
+    performance = pd.DataFrame(columns=['cpu_percent', 'memory_mib'])
+
+    for station in data_list:
+        path = f"{data_path}/{station}"
+
+        for date in os.listdir(path):
+            model = MKWIForestBatchPipeline(
+                score_threshold=0.8,
+                alpha=0.05,
+                slope_threshold=0.1,
+                window_size=window_size)
+            df = merge_data(f"{path}/{date}")
+
+            current_size = 0
+            for _, x in df.iterrows():
+                if samples >= MAX_SAMPLES:
+                    performance = performance[performance['cpu_percent'] != 0]
+                    cpu_max = performance['cpu_percent'].max()
+                    cpu_mean = performance['cpu_percent'].mean()
+                    memory_max = performance['memory_mib'].max()
+
+                    # Dump results into a log file
+                    pd.DataFrame({
+                        'cpu_max': [cpu_max],
+                        'cpu_mean': [cpu_mean],
+                        'memory_max': [memory_max]
+                    }).to_csv(os.path.join(os.getcwd(), f"./{window_size}_performance.csv"), index=False)
+
+                    return
+
+                if not model.warm:
+                    _ = model.update(x['value'])
+                    continue
+
+                current_size += 1
+                if current_size == window_size:
+                    current_size = 0
+                    monitor = Monitor(pid, INTERVAL, performance)
+                    monitor.start()
+
+                    _ = model.update(x['value'])
+
+                    monitor.stop()
+                    monitor.join()
+
+                    samples += 1
+                else:
+                    _ = model.update(x['value'])
+
+
 def main():
     window_sizes = [32, 64, 128, 256]
+    pid = os.getpid()
 
     data_path = "./labeled_data"
 
     data_list = [x for x in os.listdir(data_path) if os.path.isdir(f"{data_path}/{x}")]
     for window_size in window_sizes:
-        samples = 0
-        for station in data_list:
-            path = f"{data_path}/{station}"
-
-            for date in os.listdir(path):
-                model = MKWIForestBatchPipeline(
-                    score_threshold=0.8,
-                    alpha=0.05,
-                    slope_threshold=0.1,
-                    window_size=window_size)
-                df = merge_data(f"{path}/{date}")
-
-                current_size = 0
-                for _, x in df.iterrows():
-                    if samples >= MAX_SAMPLES:
-                        break
-                    current_size += 1
-                    if current_size == window_size:
-                        current_size = 0
-                        pid = os.getpid()
-                        monitor = Monitor(pid, 0.01)
-                        monitor.start()
-
-                        _ = model.update(x['value'])
-
-                        monitor.stop()
-                        monitor.join()
-
-                        samples += 1
-                    else:
-                        _ = model.update(x['value'])
+        print("MEASURING WINDOW SIZE: ", window_size)
+        get_performance(data_list, data_path, window_size, pid)
+        input("Press Enter to continue...")
 
 
 if __name__ == '__main__':
--- monitor.py
+++ monitor.py
@@ -1,24 +1,26 @@
-import os
-import time
 import threading
+import time
+
+import pandas as pd
 import psutil
 
 
 class Monitor(threading.Thread):
-    def __init__(self, pid, interval):
+    def __init__(self, pid, interval, performance_df):
         super().__init__()
         self._process = psutil.Process(pid)
         self._interval = interval
-        self._results = []
         self._stop_event = threading.Event()
+        self._performance_df = performance_df
 
     def run(self):
         while not self._stop_event.is_set():
             cpu_percent = self._process.cpu_percent()
             memory_mib = self._process.memory_info().rss / 1024 / 1024
-            print(f"CPU: {cpu_percent:.2f}%, Memory: {memory_mib:.2f} MiB")
-            self._results.append((cpu_percent, memory_mib))
+            self._performance_df.loc[len(self._performance_df)] = [cpu_percent, memory_mib]
             time.sleep(self._interval)
 
     def stop(self):
         self._stop_event.set()
+
+
