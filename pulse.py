c286c31eb3e0ac207c9d5c2e3881a4c249735f94
Bark Sanson <barksansondev@gmail.com>
2024-07-29 11:26:18 +0200

    Better blocks

--- pulse.py
+++ pulse.py
@@ -8,11 +8,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(TRIGGER_PIN, GPIO.OUT)
 
 
-def generate_trigger_pulse():
+def generate_trigger_pulse(continued=False):
     GPIO.output(TRIGGER_PIN, GPIO.HIGH)
     time.sleep(0.1)
     GPIO.output(TRIGGER_PIN, GPIO.LOW)
-    time.sleep(0.1)
+    time.sleep(0.1 if continued else 0.01)
 
 
 def clean():
