73a66c122a9043280bbfa7b89014fb9fc7e3df1c
Bark Sanson <barksansondev@gmail.com>
2024-09-09 14:27:32 +0200

    Better one by one

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ import numpy as np
 import pulse
 from online_outlier_detection.pipelines import MKWIForestBatchPipeline
 
-SAMPLES = 10
-
 def main():
     pulse.generate_trigger_pulse()
     window_size = int(sys.argv[1])
 
     blocks = []
 
-    for _ in range(SAMPLES):
-        model = MKWIForestBatchPipeline(
-            score_threshold=0.8,
-            alpha=0.05,
-            slope_threshold=0.1,
-            window_size=window_size)
-        
-        pulse.generate_trigger_pulse()
-        for b in blocks:
-            for x in b:
-                _ = model.update(x)
+    model = MKWIForestBatchPipeline(
+        score_threshold=0.8,
+        alpha=0.05,
+        slope_threshold=0.1,
+        window_size=window_size)
+    
+    pulse.generate_trigger_pulse()
+    for b in blocks:
+        for x in b:
+            _ = model.update(x)
+    pulse.generate_trigger_pulse()
 
     pulse.clean()
 
