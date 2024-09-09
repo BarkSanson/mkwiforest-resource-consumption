8418a0bcc666913ca768a347749863d260dc0222
Bark Sanson <barksansondev@gmail.com>
2024-09-09 11:17:08 +0200

    Debug

--- online_outlier_detection/pipelines/mkwiforestbatchpipeline.py
+++ online_outlier_detection/pipelines/mkwiforestbatchpipeline.py
@@ -35,6 +35,7 @@ class MKWIForestBatchPipeline(BatchDetectorPipeline):
 
         if self.drift_detector.detect_drift(self.window.get(), self.reference_window):
             self._retrain()
+            print("Drift")
 
             generate_trigger_pulse()
             scores = np.abs(self.model.score_samples(self.reference_window.reshape(-1, 1)))
@@ -44,6 +45,7 @@ class MKWIForestBatchPipeline(BatchDetectorPipeline):
             return scores, labels
 
         generate_trigger_pulse()
+        print("No Drift")
         scores = np.abs(self.model.score_samples(self.window.get().reshape(-1, 1)))
         labels = np.where(scores > self.score_threshold, 1, 0)
 
